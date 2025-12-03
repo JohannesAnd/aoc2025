6f0a5329af6010a4a5ebd07bd52ca316316ce2e7
Johannes Andersen <ja@vitalthings.com>
2025-12-03 09:04:34 +0100

    Solution day three

---
+++ day03/day03.go
@@ -0,0 +1,81 @@
+package day02
+
+import (
+	"aoc2025/utils"
+	"math"
+)
+
+func findLargest(str string, start int, end int) (uint8, int) {
+	var largest uint8 = 0
+	index := -1
+
+	for i := start; i < end; i++ {
+		// Account for ASCII code offset
+		value := str[i] - 48
+
+		if value > largest {
+			largest = value
+			index = i
+		}
+
+		if value == 9 {
+			break
+		}
+	}
+
+	return largest, index
+}
+
+func findLargestNumberOfSize(str string, size int) int {
+	result := 0
+
+	index := -1
+
+	for i := range size {
+		posFromEnd := size - i - 1
+
+		value, newIndex := findLargest(str, index+1, len(str)-posFromEnd)
+
+		index = newIndex
+
+		result += int(value) * int(math.Pow10(posFromEnd))
+	}
+
+	return result
+}
+
+func Part1() int {
+	lr, _ := utils.NewLineReader("day03.txt")
+
+	result := 0
+
+	for {
+		line, ok := lr.Next()
+
+		if !ok {
+			break
+		}
+
+		result += findLargestNumberOfSize(line, 2)
+	}
+
+	return result
+}
+
+func Part2() int {
+	lr, _ := utils.NewLineReader("day03.txt")
+
+	result := 0
+
+	for {
+		line, ok := lr.Next()
+
+		if !ok {
+			break
+		}
+
+		result += findLargestNumberOfSize(line, 12)
+	}
+
+	return result
+}
---
+++ day03/day03_test.go
@@ -0,0 +1,19 @@
+package day02
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_part1(t *testing.T) {
+	result := Part1()
+
+	assert.Equal(t, 16973, result)
+}
+
+func Test_part2(t *testing.T) {
+	result := Part2()
+
+	assert.Equal(t, 168027167146027, result)
+}
