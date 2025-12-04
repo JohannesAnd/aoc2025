49258da75cff03f209b7a343fd2d090af71119cc
Johannes Andersen <ja@vitalthings.com>
2025-12-04 09:55:30 +0100

    Solution day four

---
+++ day04/day04.go
@@ -0,0 +1,100 @@
+package day04
+
+import (
+	"os"
+)
+
+const newline byte = 10
+const dot byte = 46
+const at byte = 64
+
+func findFirstIndex(data []byte, value byte) (int, bool) {
+	for i := 0; i < len(data); i++ {
+		if data[i] == value {
+			return i, true
+		}
+	}
+
+	return -1, false
+}
+
+func getValue(data []byte, width int, y int, x int) (byte, bool) {
+	index := y*(width+1) + x
+
+	if y >= 0 && x >= 0 && x < width && index < len(data) {
+		return data[index], true
+	}
+
+	return 0, false
+}
+
+func countNeighbors(data []byte, width int, y int, x int) int {
+	count := 0
+
+	for _, y2 := range []int{y - 1, y, y + 1} {
+		for _, x2 := range []int{x - 1, x, x + 1} {
+			if x2 == x && y2 == y {
+				continue
+			}
+
+			value, found := getValue(data, width, y2, x2)
+
+			if found && value == at {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+func getAccessible(data []byte) []int {
+	width, _ := findFirstIndex(data, newline)
+
+	result := make([]int, 0)
+
+	for i, d := range data {
+		if d != at {
+			continue
+		}
+
+		y := i / (width + 1)
+		x := i % (width + 1)
+
+		n := countNeighbors(data, width, y, x)
+
+		if n < 4 {
+			result = append(result, i)
+		}
+	}
+
+	return result
+}
+
+func Part1() int {
+	data, _ := os.ReadFile("day04.txt")
+
+	return len(getAccessible(data))
+}
+
+func Part2() int {
+	data, _ := os.ReadFile("day04.txt")
+
+	removed := 0
+
+	for {
+		accessible := getAccessible(data)
+
+		if len(accessible) == 0 {
+			break
+		}
+
+		removed += len(accessible)
+
+		for _, i := range accessible {
+			data[i] = dot
+		}
+	}
+
+	return removed
+}
---
+++ day04/day04_test.go
@@ -0,0 +1,54 @@
+package day04
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_getValue_1(t *testing.T) {
+	d := []byte{1, 2, 10, 4, 5, 10, 7, 8, 10}
+
+	val, found := getValue(d, 2, 0, 1)
+
+	assert.Equal(t, true, found)
+	assert.Equal(t, byte(2), val)
+}
+
+func Test_getValue_2(t *testing.T) {
+	d := []byte{1, 2, 10, 4, 5, 10, 7, 8, 10}
+
+	val, found := getValue(d, 2, 1, 1)
+
+	assert.Equal(t, true, found)
+	assert.Equal(t, byte(5), val)
+}
+
+func Test_getValue_3(t *testing.T) {
+	d := []byte{1, 2, 10, 4, 5, 10, 7, 8, 10}
+
+	_, found := getValue(d, 2, -1, -1)
+
+	assert.Equal(t, false, found)
+}
+
+func Test_getValue_4(t *testing.T) {
+	d := []byte{1, 2, 10, 4, 5, 10, 7, 8, 10}
+
+	val, found := getValue(d, 2, 2, 1)
+
+	assert.Equal(t, true, found)
+	assert.Equal(t, byte(8), val)
+}
+
+func Test_part1(t *testing.T) {
+	result := Part1()
+
+	assert.Equal(t, 1495, result)
+}
+
+func Test_part2(t *testing.T) {
+	result := Part2()
+
+	assert.Equal(t, 8768, result)
+}
